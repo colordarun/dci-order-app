f2c90754ea76f17c579f7f0e6964570a497cdee7
colordarun <colordarun@gmail.com>
2026-06-18 17:49:24 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ const PRODUCTS = {
     { code: 'STCKR-SK10-DCI', name: 'DCI스킨용 진단 스티커*10 / DCI-Skin Diagnostic Sticker *10',       spec: '10개 묶음', priceL: 30000, priceS: 33000, priceDefault: 36000 },
   ],
   bottles: [
-    { code: 'BTL-FDN', name: '파운데이션 용기 / Foundation Container', spec: '30g(펌프형)', priceL: 2200, priceS: 2700, priceDefault: 3000 },
-    { code: 'BTL-LGS', name: '립글로스 용기 / Lip Gloss Container',    spec: '5ml',        priceL: 1200, priceS: 1600, priceDefault: 2000 },
+    { code: 'BTL-FDN', name: '파운데이션 용기 / Foundation Container', spec: '20ml(펌프형)_약25g', priceL: 2200, priceS: 2700, priceDefault: 3000 },
+    { code: 'BTL-LGS', name: '립글로스 용기 / Lip Gloss Container',    spec: '5ml_약6g',        priceL: 1200, priceS: 1600, priceDefault: 2000 },
   ],
 };
 
